2a79420c86297659f0677771d1c4e3e9827145c1
Guillaume <g.suppin@mailo.com>
2020-04-02 10:40:20 +0200

    creation vue blade enfants.index

--- resources/views/enfants/index.blade.php
+++ resources/views/enfants/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
 @section('content')
-    liste des enfants
+Eos iure eligendi. Sint praesentium quo itaque deserunt neque voluptatem. Eos iusto facilis ut est aliquam. Corrupti nulla vel ea deserunt.
+ 
+Eius incidunt corrupti neque dolores vitae repellendus et. Est in laborum minus voluptates consequatur eaque sapiente explicabo et. Et magni architecto id quaerat enim sint voluptas qui. Beatae voluptates quas.
+ 
+Dolore odio ut repellendus debitis dolores quidem eum qui nulla. Placeat saepe totam commodi eligendi voluptas placeat. Nihil quasi temporibus. Id voluptatum expedita debitis ea esse eum repellat voluptate. Aut quas exercitationem voluptas sit. Aut corrupti possimus sed.
 @endsection
